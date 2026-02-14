4bd349d6561ca2434d04da1d30dd49a8b831b0fa
estasney <estasney@users.noreply.github.com>
2026-02-14 13:50:17 -0500

    GH deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import {fileURLToPath} from "node:url";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteSingleFile()],
   assetsInclude: ["./src/assets/**"],
   resolve: {
@@ -14,8 +14,10 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: "docs",
+    emptyOutDir: true,
     cssCodeSplit: false,
-    assetsInlineLimit: 10_000_000, // inline assets (bytes); tune as needed
+    assetsInlineLimit: 10_000_000,
     rolldownOptions: {
       output: {
         inlineDynamicImports: true, // single JS chunk (requires single input)
